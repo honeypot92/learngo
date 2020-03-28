228489c3e7e2e04545fd2d6cee0ff51d58cc20ee
BAEDANG <hyunjun0803@gmail.com>
2020-03-28 20:16:04 +0900

    url

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-	"github.com/honeypot92/learngo/mydict"
+	"errors"
+	"fmt"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	dictionary.Add("first", "first word")
-	dictionary.Update("fist", "new definition")
+var errRequestFailed = errors.New("Request Failed!")
 
+func main() {
+	urls := []string{
+		"https://www.facebook.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://www.instagram.com/",
+		"https://soundcloud.com/",
+		"https://www.naver.com/",
+		"https://www.zum.com/",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking Url:", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
