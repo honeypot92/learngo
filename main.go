c8fdbf482cf4c722b274d7bf33dfd67d440bcd11
BAEDANG <hyunjun0803@gmail.com>
2020-04-13 18:08:45 +0900

    echo test

--- main.go
+++ main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
-	"os"
-	"strings"
-
-	"github.com/honeypot92/learngo/scrapper"
-
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
-const fileName string = "jobs.csv"
+// const fileName string = "jobs.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
-func handleScrape(c echo.Context) error {
-	defer os.Remove(fileName)
-	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	scrapper.Scrape(term)
-	return c.Attachment(fileName, fileName)
-}
+// func handleScrape(c echo.Context) error {
+// defer os.Remove(fileName)
+// term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+// scrapper.Scrape(term)
+// return c.Attachment(fileName, fileName)
+// }
 
 func main() {
 	e := echo.New()
+	// g := e.Group("/admin")
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "[${time_rfc3339}] ${method} ${error} ${path} ${latency}" + "\n",
+	}))
+	// e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", handleHome)
-	e.POST("/scrape", handleScrape)
-	e.Logger.Fatal(e.Start(":1323"))
+	// e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":8000"))
 	//	scrapper.Scrape("term")
 }
